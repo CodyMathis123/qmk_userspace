085c0e59d7ef24d4954f370acbde58e46e978d67
Cody Mathis <cody.mathis123@gmail.com>
2025-12-23 00:42:44 -0500

    Update config.h

--- config.h
+++ config.h
@@ -5,12 +5,13 @@
 
 // Common QMK settings
 #define USB_SUSPEND_WAKEUP_DELAY 2000
-#define RP2040_BOOTLOADER_DOUBLE_TAP_RESET_TIMEOUT 1500U
+#define RP2040_BOOTLOADER_DOUBLE_TAP_RESET_TIMEOUT 1000U
+
 // Space saving options
 #define LAYER_STATE_8BIT
 #define NO_ACTION_ONESHOT
 // Tap-hold settings
-#define TAPPING_TERM 220
+#define TAPPING_TERM 185
 #define QUICK_TAP_TERM 105
 #ifdef CONVERT_TO_KB2040
 #   define SHIFT_TAP_TERM 165
@@ -25,30 +26,22 @@
 #endif
 
 #ifdef COMBO_ENABLE
-#   define COMBO_TERM 25
+#   define COMBO_TERM 30
 #   define EXTRA_SHORT_COMBOS
 #   define COMBO_SHOULD_TRIGGER
 #   define COMBO_ONLY_FROM_LAYER 0
 #endif
 
 #ifdef MOUSEKEY_ENABLE
-#   define MOUSEKEY_OVERLAP_RESET
-#   define MOUSEKEY_OVERLAP_INTERVAL   64
-#   define MOUSEKEY_DELAY               0 // Delay between pressing a key and cursor movement
-#   define MOUSEKEY_INTERVAL           16 // Time between cursor movements in milliseconds
-#   if defined (MK_KINETIC_SPEED)
-#       define MOUSEKEY_MOVE_DELTA     25
-#       define MOUSEKEY_INITIAL_SPEED 200
-#       define MOUSEKEY_WHEEL_INITIAL_MOVEMENTS 4
-#   else // Default accelerated
-#       define MOUSEKEY_MAX_SPEED      10
-#       define MOUSEKEY_MOVE_DELTA     10 // Step size for acceleration
-#       define MOUSEKEY_TIME_TO_MAX    80
-#   endif
-#   define MOUSEKEY_WHEEL_DELAY        16
-#   define MOUSEKEY_WHEEL_INTERVAL     30
-#   define MOUSEKEY_WHEEL_MAX_SPEED    10
-#   define MOUSEKEY_WHEEL_TIME_TO_MAX  95
+#	define MOUSEKEY_DELAY 0			// Delay between pressing a key and cursor movement
+#	define MOUSEKEY_INTERVAL 15		// Time between cursor movements in milliseconds
+#	define MOUSEKEY_MOVE_DELTA 10	// Step size for acceleration
+#	define MOUSEKEY_MAX_SPEED 9
+#	define MOUSEKEY_TIME_TO_MAX 90
+#	define MOUSEKEY_WHEEL_DELAY 16
+#	define MOUSEKEY_WHEEL_INTERVAL 30
+#	define MOUSEKEY_WHEEL_MAX_SPEED 10
+#	define MOUSEKEY_WHEEL_TIME_TO_MAX 95
 #endif
 
 #ifdef RGB_MATRIX_ENABLE
